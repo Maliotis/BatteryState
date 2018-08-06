1fbc727dab0f91f3849bf1dde3337634224b9fd5
petros <maliotis12@gmail.com>
2018-08-06 14:16:36 +0300

    Settings Activity (not completed)

--- app/src/main/java/com/maliotis/batterystate/PluginCharge.java
+++ app/src/main/java/com/maliotis/batterystate/PluginCharge.java
@@ -3,17 +3,22 @@ package com.maliotis.batterystate.BroadcastReceivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.maliotis.batterystate.Activities.MainActivity;
 import com.maliotis.batterystate.Helper;
 
 public class PluginCharge extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if (!Helper.isAppRunning(context)) {
             Intent intent1 = new Intent(context, MainActivity.class);
             context.startActivity(intent1);
+            Log.d("Receiver",intent.getAction());
         }
+        Intent intent1 = new Intent(context, MainActivity.class);
+        context.startActivity(intent1);
     }
 
 
